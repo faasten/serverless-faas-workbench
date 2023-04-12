5ca2af52a34a0da22d8053decc4f2e1b4c7ee59c
Yue Tan <yuetan@cs.princeton.edu>
2023-04-12 01:55:01 -0500

    add jsons; add run_benchmarks.sh

---
+++ openwhisk/run_benchmarks.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# TODO
+# setup files for image/video funtions
+
+jsons=($(ls jsons))
+for json in "${jsons[@]}"; do
+    filename=$(basename -- "$json")
+    action="${filename%.*}"
+    wsk action invoke $action --param-file jsons/$filename -i -r
+done
