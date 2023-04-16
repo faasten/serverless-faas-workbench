e5963616a112681cb2983f9124c0afce1119f40a
Gongqi Huang <gongqih@hotmail.com>
2023-04-16 14:46:15 -0400

    add script for wskresult21*

---
+++ openwhisk/show.py
@@ -0,0 +1,62 @@
+import json, sys, numpy
+
+names = [
+    "chameleon",
+    "float_operation",
+    "image_processing",
+    "linpack",
+    "matmul",
+    #  "ml_video_face_detection",
+    #  "model_training",
+    "pyaes",
+    "video_processing",
+]
+
+
+long_names = [
+    "ml_video_face_detection",
+    "model_training",
+]
+
+file = sys.argv[1]
+long_file = sys.argv[2]
+
+RUNS = 21
+
+def process(file, names):
+
+    with open(file) as f:
+
+        jsons = json.load(f)
+        assert len(jsons) % RUNS == 0
+        n = len(jsons) // RUNS
+
+        grouped = [
+            jsons[i:len(jsons):n] if RUNS == 1 else jsons[i:len(jsons):n][1:]
+            for i in range(n)
+        ]
+
+        grouped_times = [
+            [
+                j['timestamps']['finishing_time'] - j['timestamps']['starting_time']
+                for j in g
+            ]
+            for g in grouped
+        ]
+
+        avgs = [
+            sum(times) / len(times)
+            for times in grouped_times
+        ]
+
+        for n, ts in zip(names, grouped_times):
+            avg = sum(ts) / len(ts)
+            std = numpy.std(ts)
+            print(n, avg, std)
+
+
+process(file, names)
+process(long_file, long_names)
+
+
+
