adbfa59aa7269b67b0638fe065f788a1c034bebc
Gongqi Huang <gongqih@hotmail.com>
2024-04-10 15:01:27 -0400

    nits

--- openwhisk/build_publish_docker_image.sh
+++ openwhisk/build_publish_docker_image.sh
@@ -1,8 +1,19 @@
 #!/bin/bash -x
+
 ACTIONDIR=$(dirname $(realpath $0))/cpu-memory
 actions=($(ls $ACTIONDIR))
-for action in "${actions[@]}"; do
-    docker build -t py3:$action $ACTIONDIR/$action
-    docker tag py3:$action yuetan/py3:$action
-    docker push yuetan/py3:$action
-done
+
+if [[ -z "$1" ]]
+then
+    for action in "${actions[@]}"; do
+        docker build -t py3:$action $ACTIONDIR/$action
+        docker tag py3:$action somefish1/py3:$action
+        docker push somefish1/py3:$action
+    done
+    exit 0
+fi
+
+action=$1
+docker build -t py3:$action $ACTIONDIR/$action
+docker tag py3:$action somefish1/py3:$action
+docker push somefish1/py3:$action
--- openwhisk/create_actions.sh
+++ openwhisk/create_actions.sh
@@ -3,8 +3,10 @@ ACTIONDIR=$(dirname $(realpath $0))/cpu-memory
 actions=($(ls $ACTIONDIR))
 
 for action in "${actions[@]}"; do
-    wsk action update $action $ACTIONDIR/$action/function.py --docker yuetan/py3:$action -i
+    wsk action update $action $ACTIONDIR/$action/function.py --memory 2048 --timeout 120000 --docker somefish1/py3:$action -i
 done
+
+# Note: docker pull image somefish1/py3 -a on the invoker machine to sync images
 
 
 #for action in "${actions[@]}"; do
